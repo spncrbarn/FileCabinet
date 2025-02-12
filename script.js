03bdbf4168fc168ab7cdb49a78e316e0e78cc066
spncrbarn <spncrbarn@gmail.com>
2025-02-12 13:57:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.onload = function() {
     const folders = document.querySelectorAll('.folder');
     folders.forEach(folder => {
-        const randomTop = Math.random() * (window.innerHeight - 100); // Adjust to allow space for images
-        const randomLeft = Math.random() * (window.innerWidth - 150); // Adjust to allow space for images
+        const randomTop = Math.random() * (window.innerHeight - 100); // Adjust to keep inside window
+        const randomLeft = Math.random() * (window.innerWidth - 150); // Adjust to keep inside window
         folder.style.top = `${randomTop}px`;
         folder.style.left = `${randomLeft}px`;
     });
