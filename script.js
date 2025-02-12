d2bc739862f471405122c18fbced95d0bab8cf6d
spncrbarn <spncrbarn@gmail.com>
2025-02-12 13:46:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.onload = function() {
-    const folders = document.querySelectorAll('.folder-container');  // Select the folder containers
+    const folders = document.querySelectorAll('.folder');
     folders.forEach(folder => {
-        const randomTop = Math.random() * (window.innerHeight - 100); // 100 is the height of the image
-        const randomLeft = Math.random() * (window.innerWidth - 150); // 150 is the width of the image
+        const randomTop = Math.random() * (window.innerHeight - 100);
+        const randomLeft = Math.random() * (window.innerWidth - 150);
         folder.style.top = `${randomTop}px`;
         folder.style.left = `${randomLeft}px`;
     });
